6755b6b5fd8016c56e5244ffd7589261af973112
Nathan Kim <nathankim137311@gmail.com>
2022-02-18 20:57:10 -0800

    project setup with tailwind and react-router

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
