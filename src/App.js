ba1512760b74d2ca9d6696b9c11533300ccaf0cc
Nathan Kim <nathankim137311@gmail.com>
2022-03-03 15:13:52 -0800

    basic styling for About page completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Intro from './components/Intro';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import About from './components/About'; 
 import Projects from './components/Projects';
 
 function App() {
   return (
     <div className='relative bg-[#1A1A1A] bg-clouds-pattern'>
       <Intro />
+      <About />
       <Projects />
       <Contact />
       <Footer />
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Avatar from '../assets/png/myAvatar.png';
+
+export default function About() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen px-6 text-white'>
+        <h2 className='text-3xl font-bold text-center text-white'>About</h2>
+        <div className='my-10'>
+            <img className='w-40 h-40 rounded-full' src={Avatar} alt="" />
+        </div>
+            <span className='mb-4'>Based in Bothell, WA</span>
+        <div className=''>
+            <p className='mb-4 text-left'>Nathan Kim is a who has a thirst for all things programming. He has went from not having written a single line of code to creating his own full stack project in five months. He is a <strong className='text-[#0D9488]'>creative and logical person</strong>, who has an eye for design.</p>
+            <p className='mb-4 text-left'>When he is not coding, he spends his free time hanging out with friends on Discord, working out and watching Football.</p>
+            <p className='border-l-4 border-[#0D9488] px-3 py-2 bg-[#1A1A1A] text-gray-400'>I wish I could by me a spaceship and fly past the sky - Kanye West</p>
+        </div>
+
+    </div>
+  )
+}
