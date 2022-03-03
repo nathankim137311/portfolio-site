a264a587ccdbd4168783d518899621f4ba90a0ef
Nathan Kim <nathankim137311@gmail.com>
2022-03-03 15:19:24 -0800

    added cyan as new accent color

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,9 @@ export default function About() {
         </div>
             <span className='mb-4'>Based in Bothell, WA</span>
         <div className=''>
-            <p className='mb-4 text-left'>Nathan Kim is a who has a thirst for all things programming. He has went from not having written a single line of code to creating his own full stack project in five months. He is a <strong className='text-[#0D9488]'>creative and logical person</strong>, who has an eye for design.</p>
+            <p className='mb-4 text-left'>Nathan Kim is a who has a thirst for all things programming. He has went from not having written a single line of code to creating his own full stack project in five months. He is a <strong className='text-[#15ebd9]'>creative and logical person</strong>, who has an eye for design.</p>
             <p className='mb-4 text-left'>When he is not coding, he spends his free time hanging out with friends on Discord, working out and watching Football.</p>
-            <p className='border-l-4 border-[#0D9488] px-3 py-2 bg-[#1A1A1A] text-gray-400'>I wish I could by me a spaceship and fly past the sky - Kanye West</p>
+            <p className='border-l-4 border-[#15ebd9] px-3 py-2 bg-[#1A1A1A] text-gray-400'>I wish I could by me a spaceship and fly past the sky - Kanye West</p>
         </div>
 
     </div>
