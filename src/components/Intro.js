421f9d7b53c51ec520e916bbeb4f1ba1265f9c8a
Nathan Kim <nathankim137311@gmail.com>
2022-03-09 22:58:35 -0800

    installed framer-motion and added animations to intro and skills sections

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Link } from 'react-scroll';
 import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs';
 import arrowDown from '../assets/gif/arrow-down.gif';
+import { motion } from 'framer-motion';
 
 export default function Intro() {
+
   return (
     <div className='relative flex flex-col justify-center h-screen'>
       <div className='flex flex-col items-start px-6 text-white xs:items-center xs:max-w-2xl sm:mx-auto z-[1]'>
-        <div>
-          <h1 className='text-6xl text-transparent font-archivo xs:text-center bg-clip-text bg-gradient-to-tl from-[#A400FF] to-[#ED018C]'>Nathan Kim.</h1>
-          <p className='mt-4 mb-8 text-lg text-slate-300 xs:text-xl xs:text-center'>I am a <strong>software developer</strong> that builds and designs responsive web applications.</p>
-        </div>
-        <div className='flex items-center w-full sm:w-1/2 drop-shadow-lg xs:justify-center'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeIn", duration: 1 }}
+        >
+          <h1 className='text-6xl text-transparent font-archivo xs:text-center bg-clip-text bg-gradient-to-r from-[#A400FF] to-[#ED018C]'>Nathan Kim.</h1>
+          <p className='mt-4 mb-8 text-lg text-slate-300 xs:text-xl xs:text-center'>I am a <strong className='font-normal underline decoration-dotted underline-offset-[6px] decoration-[#ED018C] decoration-[3px]'>software developer</strong> that builds and designs responsive web applications.</p>
+        </motion.div>
+        <motion.div 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeIn", duration: 1, delay: .35 }}
+          className='flex items-center w-full sm:w-1/2 drop-shadow-lg xs:justify-center'
+        >
           <Link className='w-1/2 mr-4 text-center md:mr-8 purple-btn hover:scale-105 hover:cursor-pointer' to='contact' spy={true} smooth={true}>Contact</Link>
           <div className='flex px-2 py-2 bg-gray-900 rounded-full md:px-4 md:py-3 text-slate-400'>
             <a href='https://github.com/nathankim137311'>
@@ -24,7 +35,7 @@ export default function Intro() {
               <BsTwitter className='w-6 h-6 mx-2 transition-all duration-200 ease-linear hover:text-[#15ebd9] hover:scale-110'/>
             </a>
           </div>
-        </div>
+        </motion.div>
       </div>
       <img className='absolute w-12 h-auto -translate-x-1/2 md:w-16 bottom-10 left-1/2' src={arrowDown} alt="Animated down arrow" />
     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AiFillHtml5 } from 'react-icons/ai'
 import { DiCss3, DiJavascript1 } from 'react-icons/di'
 import { 
@@ -10,17 +10,62 @@ import {
 } from 'react-icons/si'
 import { ImGit } from 'react-icons/im'
 import { IoLogoNodejs } from 'react-icons/io'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 export default function Skills() {
+    const {ref, inView} = useInView();
+    const animation = useAnimation();
+
+    useEffect(() => {
+        if(inView) {
+            animation.start({
+                opacity: 1,
+                x: '0',
+                transition: {
+                    type: 'spring',
+                    ease: 'linear',
+                    duration: .5,
+                    delay: .25
+                }
+            })
+        }
+        if(!inView) {
+            animation.start({ opacity: 0, x: '-100vw'});
+        }
+        console.log(inView);
+    }, [inView])
+
+    const list = {
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 },
+    }
+      
+    const item = {
+        visible: { opacity: 1, x: 0 },
+        hidden: { opacity: 0, x: -100 },
+    }
+
   return (
     <div id='skills' className='relative h-screen bg-architect'>
-        <div className='absolute flex flex-col items-center w-full max-w-5xl px-6 py-12 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 sm:px-10'>
-            <div className='w-full mb-4'>
+        <div ref={ref} className='absolute flex flex-col items-center w-full max-w-5xl px-6 py-12 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 sm:px-10'>
+            <motion.div 
+                animate={animation}
+                className='w-full mb-4'
+            >
                 <h2 className='w-full mb-4 text-base font-normal text-left underline text-slate-500 font-archivo underline-offset-8 decoration-2'>Skills</h2>
                 <span className='text-xl font-bold sm:text-2xl text-slate-100'>Technologies and languages</span>
-            </div>
-            <div className='w-full px-6 xs:px-12 sm:px-16 py-12 bg-[#0E131F] flex flex-row justify-center shadow-lg rounded-xl'>
-                <ul className='grid w-full max-w-4xl grid-cols-2 text-sm text-slate-200 gap-y-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 lg:gap-y-12'>
+            </motion.div>
+            <motion.div 
+                animate={animation}
+                className='w-full px-6 xs:px-12 sm:px-16 py-12 bg-[#0E131F] flex flex-row justify-center shadow-lg rounded-xl'
+            >
+                <motion.ul 
+                    initial='hidden'
+                    animate='visible'
+                    variants={list}
+                    className='grid w-full max-w-4xl grid-cols-2 text-sm text-slate-200 gap-y-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 lg:gap-y-12'
+                >
                     <li className='flex flex-col items-center'>
                         <div className='skill-badge bg-[#E44D26] text-white'>
                             <AiFillHtml5 className='w-10 h-10'/>
@@ -81,8 +126,8 @@ export default function Skills() {
                         </div>
                         <span>TailwindCSS</span>
                     </li>
-                </ul>
-            </div>
+                </motion.ul>
+            </motion.div>
         </div>
     </div>
   )
