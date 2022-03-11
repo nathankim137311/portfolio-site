a634c7356297f6a99a992e823efed3c624397b4f
Nathan Kim <nathankim137311@gmail.com>
2022-03-10 20:58:03 -0800

    added more projects to array

--- src/config/projectsArr.js
+++ src/config/projectsArr.js
@@ -2,11 +2,13 @@ import legoCloneHome from '../assets/png/lego-clone-home.png';
 import starWarsApiImg from '../assets/png/lego-star-wars-api.png';
 import weatherAppImg from '../assets/png/weather-app.png';
 import restaurantSiteImg from '../assets/png/korean-street.png';
+import discordLogo from '../assets/png/discord_logo.png'
+import todoImg from '../assets/png/todo.png'
 
 export const projects = [
     {
         id: 1,
-        title: 'Lego Clone',
+        title: 'Lego Shop',
         query: 'lego-clone',
         img_url: legoCloneHome,
         tech: 'MongoDB | Express | React | Node | Git | TailwindCSS',
@@ -47,5 +49,27 @@ export const projects = [
         github_url: 'https://github.com/nathankim137311/restaurant-page',
         type: 'MPA',
         description: 'A custom-designed restaurant site that utilizes Google Maps API.'
+    },
+    {
+        id: 5,
+        title: 'Smash Bot',
+        query: 'smash-bot',
+        img_url: discordLogo,
+        tech: 'JavaScript | Node | Docker | SQL | Git | Postman',
+        live_url: '',
+        github_url: 'https://github.com/nathankim137311/LiquidHacksBot',
+        type: 'BOT',
+        description: 'A game that allows players to fight with their favorite smash characters against their friends.'
+    },
+    {
+        id: 6,
+        title: 'Todo App',
+        query: 'todo-app',
+        img_url: todoImg,
+        tech: 'HTML | SCSS | JavaScript | Git | Webpack',
+        live_url: 'https://nathankim137311.github.io/todo-app',
+        github_url: 'https://github.com/nathankim137311/todo-app',
+        type: 'SPA',
+        description: 'An amped up Todo List dashboard. Users can perform basic CRUD operations.'
     }
 ]
